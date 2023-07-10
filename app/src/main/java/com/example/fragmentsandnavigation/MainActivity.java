d80a082901939b3967ce8f8f9bae694ff04d249e
aliShykh20 <ahmadmubashar373@gmail.com>
2023-07-10 10:13:18 +0500

    Added quiz in the drawer

--- app/src/main/java/com/example/fragmentsandnavigation/MainActivity.java
+++ app/src/main/java/com/example/fragmentsandnavigation/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.fragmentsandnavigation;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -18,48 +26,67 @@ import com.example.fragmentsandnavigation.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration mAppBarConfiguration;
-    private ActivityMainBinding binding;
-
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        if(drawerLayout == null){
+            Toast.makeText(this, "Emohjwvkvkv", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            // Setting toolbar
+            setSupportActionBar(toolbar);
 
-        setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setOpenableLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
-    }
+            // Making it so it can be added to toggle the drawer
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+            // Adding listener to drawer and passing toggle
+            drawerLayout.addDrawerListener(toggle);
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+            // Managing state of drawer open or close
+            toggle.syncState();
+
+            // Click listener for items in the drawer
+            navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    int id = item.getItemId();
+                    boolean chk = true;
+
+                    if(!chk){
+                        return true;
+                    }
+
+                    if(id == R.id.nav_quiz){
+                        FragmentManager fm = getSupportFragmentManager();
+                        FragmentTransaction ft = fm.beginTransaction();
+
+                        ft.add(R.id.nav_host_fragment_content_main, new Quiz());
+                        ft.commit();
+                        chk = false;
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                    }
+
+                    if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                        // Close the drawer and return true to consume the event
+                        drawerLayout.closeDrawers();
+                        return true;
+                    }
+                    // return will be true to view the selection
+                    return true;
+                }
+            });
+        }
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/fragmentsandnavigation/Quiz.java
@@ -0,0 +1,125 @@
+package com.example.fragmentsandnavigation;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class Quiz extends Fragment {
+
+    private TextView letterTextView, answerTextView;
+    private char[] skyLetters = {'b', 'd', 'f', 'h', 'k', 'l', 't'};
+    private char[] grassLetters = {'g', 'j', 'p', 'q', 'y'};
+    private char[] rootLetters = {'a', 'c', 'e', 'i', 'm', 'n', 'o', 'r', 's', 'u', 'v', 'w', 'x', 'z'};
+    private String answerString = "";
+
+    public Quiz() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_quiz, container, false);
+
+        letterTextView = view.findViewById(R.id.letter_text_view);
+        letterTextView.setText(getRandomLetter());
+
+        answerTextView = view.findViewById(R.id.answer_text_view);
+
+        Button skyButton = view.findViewById(R.id.sky_button);
+        skyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (answerString == "Sky Letter") {
+                    answerTextView.setText("Awesome your answer is right");
+                } else {
+                    answerTextView.setText("Incorrect! the answer is " + answerString);
+                }
+
+                // Wait for 5 seconds and create a new question
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        letterTextView.setText(getRandomLetter());
+                        answerTextView.setText("");
+                    }
+                }, 5000); // 5000 milliseconds = 5 seconds
+            }
+        });
+
+        Button grassButton = view.findViewById(R.id.grass_button);
+        grassButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (answerString == "Grass Letter") {
+                    answerTextView.setText("Awesome your answer is right");
+                } else {
+                    answerTextView.setText("Incorrect! the answer is " + answerString);
+                }
+                // Wait for 5 seconds and create a new question
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        letterTextView.setText(getRandomLetter());
+                        answerTextView.setText("");
+                    }
+                }, 5000); // 5000 milliseconds = 5 seconds
+            }
+        });
+
+        Button rootButton = view.findViewById(R.id.root_button);
+        rootButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (answerString == "Root Letter") {
+                    answerTextView.setText("Awesome your answer is right");
+                } else {
+                    answerTextView.setText("Incorrect! the answer is " + answerString);
+                }
+                // Wait for 5 seconds and create a new question
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        letterTextView.setText(getRandomLetter());
+                        answerTextView.setText("");
+                    }
+                }, 5000); // 5000 milliseconds = 5 seconds
+            }
+        });
+
+
+
+        return view;
+    }
+
+    private String getRandomLetter() {
+        Random random = new Random();
+        int category = random.nextInt(3);
+        char letter;
+        switch (category) {
+            case 0:
+                letter = skyLetters[random.nextInt(skyLetters.length)];
+                answerString = "Sky Letter";
+                break;
+            case 1:
+                letter = grassLetters[random.nextInt(grassLetters.length)];
+                answerString = "Grass Letter";
+                break;
+            default:
+                letter = rootLetters[random.nextInt(rootLetters.length)];
+                answerString = "Root Letter";
+                break;
+        }
+        return String.valueOf(letter);
+    }
+}
